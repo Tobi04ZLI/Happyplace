fd0adcfd18c8b2372b7c6d29adfcac0f4ee4d80c
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-01-06 15:48:10 +0100

    verknüpfung mit Datenbank

--- index.php
+++ index.php
@@ -1,29 +1,63 @@
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "happyplace";
+
+$connection = new mysqli($servername, $user, $password, $dbname);
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/css/ol.css" type="text/css">
     <style>
+      * /*{
+        margin: 0;
+        padding: 0;
+      }*/
       .map {
-        height: 850px;
+        height: 100vh;
         width: 100%;
+        /*position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -9;*/
       }
     </style>
-    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script> 
-    <title>OpenLayers example</title>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.5.0/build/ol.js"></script>
+    <title>Project Happy Place</title>
   </head>
   <body>
-    <h2>My Map</h2>
     <div id="map" class="map"></div>
     <script type="text/javascript">
       var map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
+            /*
+["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
+["http://a.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://b.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://c.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png"]
+["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
+["http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile2.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png"]
+["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
+["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
+*/
+            source: new ol.source.XYZ({
+                urls : ["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
+            })
+
+            /*source: new ol.source.OSM()*/
+          }),
+          /*new ol.layer.Vector({
+            source: new ol.source.Vector({
+              format: new ol.format.GeoJSON(),
+              url: './assets/data/countries.geojson' // GeoCountries file from github
+            })
+          })*/
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([8.521069874592312, 47.36012686066684]),
+          center: ol.proj.fromLonLat([8.5208324, 47.360127]),
           zoom: 10
         })
       });
