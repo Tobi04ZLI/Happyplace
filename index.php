519b0c9004f0487cc316500c2457b7ff73e4ab87
Tobias Bertschi <bertschi.tobias@bluewin.ch>
2021-01-11 13:28:01 +0100

    hinzufügen von "nach Person suchen"

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php
-$servername = "localhost";
-$user = "root";
-$password = "";
-$dbname = "happyplace";
-
-$connection = new mysqli($servername, $user, $password, $dbname);
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,8 +8,9 @@ $connection = new mysqli($servername, $user, $password, $dbname);
         padding: 0;
       }*/
       .map {
-        height: 100vh;
-        width: 100%;
+        margin-top: 40px;
+        height: 800px;
+        width: 80%;
         /*position: absolute;
         top: 0;
         left: 0;
@@ -29,38 +21,104 @@ $connection = new mysqli($servername, $user, $password, $dbname);
     <title>Project Happy Place</title>
   </head>
   <body>
+  <form action = "index.php" method = "POST">
+  <input type="text" name="personsearch" value="<?php echo($_POST["personsearch"]);?>">
+  <input type="text" name="personsearch-last" value="<?php echo($_POST["personsearch-last"]);?>">
+  <button type="submit" name="submit-search">
+  suchen
+  </button>
+  </form>
+
     <div id="map" class="map"></div>
     <script type="text/javascript">
       var map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
-            /*
-["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
-["http://a.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://b.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png","http://c.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png"]
-["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
-["http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile2.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile3.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png","http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png"]
-["http://a.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://b.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png","http://d.tile.stamen.com/watercolor/{z}/{x}/{y}.png"]
-["http://a.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png","http://c.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"]
-*/
+
             source: new ol.source.XYZ({
                 urls : ["http://a.tile.openstreetmap.org/{z}/{x}/{y}.png","http://b.tile.openstreetmap.org/{z}/{x}/{y}.png","http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"]
             })
 
             /*source: new ol.source.OSM()*/
           }),
-          /*new ol.layer.Vector({
+          new ol.layer.Vector({
             source: new ol.source.Vector({
               format: new ol.format.GeoJSON(),
               url: './assets/data/countries.geojson' // GeoCountries file from github
             })
-          })*/
+          })
         ],
         view: new ol.View({
           center: ol.proj.fromLonLat([8.5208324, 47.360127]),
           zoom: 10
         })
       });
+
+              function add_map_point(lng, lat) {
+            var vectorLayer = new ol.layer.Vector({
+                source: new ol.source.Vector({
+                    features: [new ol.Feature({
+                        geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
+                    })]
+                }),
+                style: new ol.style.Style({
+                    image: new ol.style.Icon({
+                        anchor: [0.5, 0.5],
+                        anchorXUnits: "fraction",
+                        anchorYUnits: "fraction",
+                        src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
+                    })
+                })
+            });
+            map.setView(new ol.View({
+                center: ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857'),
+                zoom: 10
+            }));
+            map.addLayer(vectorLayer);
+        }
     </script>
+
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "happyplace";
+
+$connection = new mysqli($servername, $user, $password, $dbname);
+
+if ($connection->connect_error) {
+  die("Connection failed: " . $connection->connect_error);
+}
+if (isset($_POST['submit-search'])) {
+  $searchedperson = $_POST['personsearch'];
+  $searchedperson_last = $_POST['personsearch-last'];
+  $sql_full = "SELECT * FROM apprentices WHERE prename='" . $searchedperson . "' AND lastname='$searchedperson_last';";
+  $result_full = $connection->query($sql_full);
+  $sql_appr = "SELECT prename, lastname FROM apprentices WHERE prename='" . $searchedperson . "' AND lastname='$searchedperson_last';";
+  $result_appr = $connection->query($sql_appr);
+  if ($result_full->num_rows > 0) {
+      $row_full = $result_full->fetch_array(MYSQLI_BOTH);
+      $row_appr = $result_appr->fetch_array(MYSQLI_BOTH);
+      $place_id = $row_full[3];
+      $marker_id = $row_full[4];
+      $sql_place = "SELECT latitude, longitude FROM places WHERE id=" . $place_id . ";";
+      $sql_marker = "SELECT color FROM markers WHERE id=" . $marker_id . ";";
+      $result_places = $connection->query($sql_place);
+      $result_marker = $connection->query($sql_marker);
+      $row_places = $result_places->fetch_array(MYSQLI_BOTH);
+      $row_marker = $result_marker->fetch_array(MYSQLI_BOTH);
+      echo "
+      <script type='text/javascript'>
+          add_map_point(" . $row_places[1] . ", " . $row_places[0] . ");
+      </script>";;
+  } else {
+      echo "<p id='result-id' class='result'>0 results</p>";
+  }
+
+}
+$connection->close();
+?>
+
   </body>
-</html>
+</html> 
